22e479e26938574980f2ff726ca9e39c3a6a024e
Tugay Gül <51514616+sondertg@users.noreply.github.com>
2023-01-20 02:46:44 +0300

    first commit

---
+++ src/main/java/com/itg/supplychainmanagment/SupplyChainManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.itg.supplychainmanagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplyChainManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupplyChainManagmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itg/supplychainmanagment/SupplyChainManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itg.supplychainmanagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplyChainManagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
