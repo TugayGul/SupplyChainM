ef8be53e7f0fc160fccdf82dba32f37e01f74e52
Tugay Gül <51514616+sondertg@users.noreply.github.com>
2023-01-20 05:16:51 +0300

    add db test

---
+++ src/test/java/com/itg/supplychainmanagment/DbTest.java
@@ -0,0 +1,25 @@
+package com.itg.supplychainmanagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringJUnitConfig
+@SpringBootTest
+public class DbTest {
+    @Autowired
+    private DataSource dataSource;
+
+    @Test
+    public void testConnection() throws SQLException {
+        Connection connection = dataSource.getConnection();
+        assertNotNull(connection);
+    }
+}
