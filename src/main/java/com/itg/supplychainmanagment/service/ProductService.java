d9f75c377d61c90e0806ef56720901a5bfd728e0
Tugay Gül <51514616+sondertg@users.noreply.github.com>
2023-01-20 17:07:38 +0300

    fix formatting

--- src/main/java/com/itg/supplychainmanagment/service/ProductService.java
+++ src/main/java/com/itg/supplychainmanagment/service/ProductService.java
@@ -14,6 +14,7 @@ public class ProductService {
     private ProductRepository productRepository;
 
     public Product createProduct(Product product) {
+
         return productRepository.save(product);
     }
 
