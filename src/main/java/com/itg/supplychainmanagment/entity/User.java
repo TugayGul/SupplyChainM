87e24089d30b739836152a617f1739da3e95c39e
Tugay Gül <51514616+sondertg@users.noreply.github.com>
2023-01-20 04:18:11 +0300

    fix user class

--- src/main/java/com/itg/supplychainmanagment/entity/User.java
+++ src/main/java/com/itg/supplychainmanagment/entity/User.java
@@ -1,2 +1,87 @@
-package com.itg.supplychainmanagment.entity;public class User {
+package com.itg.supplychainmanagment.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id", updatable = false, nullable = false)
+    private Long userId;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "email_address")
+    private String emailAddress;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "is_retailer")
+    private boolean isRetailer;
+
+    public User(){}
+
+    public User(Long userId, String name, String phoneNumber, String emailAddress, String password, boolean isRetailer) {
+        this.userId = userId;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.emailAddress = emailAddress;
+        this.password = password;
+        this.isRetailer = isRetailer;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isRetailer() {
+        return isRetailer;
+    }
+
+    public void setRetailer(boolean isRetailer) {
+        this.isRetailer = isRetailer;
+    }
 }
