2e6e3d807b8e1f0fdbd365b9ea26231d9a6df18c
Tugay Gül <51514616+sondertg@users.noreply.github.com>
2023-01-21 23:41:20 +0300

    add auth roles for endpoints

--- src/main/java/com/itg/supplychainmanagment/controller/ProductController.java
+++ src/main/java/com/itg/supplychainmanagment/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.itg.supplychainmanagment.controller;
 import com.itg.supplychainmanagment.entity.Product;
 import com.itg.supplychainmanagment.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,31 +15,43 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
+    @PreAuthorize("hasAnyRole('ROLE_RETAILER','ROLE_SUPPLIER')")
     @GetMapping("/{id}")
     public Product viewProductDetails(@PathVariable Long id) {
         return productService.viewProductDetails(id);
     }
 
+
+    @PreAuthorize("hasAnyRole('ROLE_RETAILER','ROLE_SUPPLIER')")
     @GetMapping("/search/{name}")
     public Product searchByProductName(@PathVariable String name) {
         return productService.searchByProductName(name);
     }
 
+
+    @PreAuthorize("hasAnyRole('ROLE_RETAILER','ROLE_SUPPLIER')")
     @GetMapping
     public List<Product> getAllProducts() {
         return productService.getAllProducts();
     }
 
+
+    @PreAuthorize(value = "hasRole('ROLE_SUPPLIER')")
     @PostMapping
     public Product createProduct(@RequestBody Product product) {
         return productService.createProduct(product);
     }
 
+
     // TODO: check this method and relative rep/service parts
+    @PreAuthorize(value = "hasRole('ROLE_SUPPLIER')")
     @PutMapping("/{productId}")
     public Product updateProduct(@PathVariable Long productId, @RequestBody Product product) {
         return productService.updateProduct(productId, product);
     }
+
+
+    @PreAuthorize(value = "hasRole('ROLE_SUPPLIER')")
     @DeleteMapping("/delete/{id}")
     public void deleteProduct(@PathVariable Long id) {
         productService.deleteProduct(id);
