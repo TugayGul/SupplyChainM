6ad2b8fe3208796d56ce9b1123731d40d37e95d2
Tugay Gül <51514616+sondertg@users.noreply.github.com>
2023-01-21 16:04:51 +0300

    small fixes

--- src/test/java/com/itg/supplychainmanagment/test/DbTest.java
+++ src/test/java/com/itg/supplychainmanagment/test/DbTest.java
@@ -1,4 +1,4 @@
-package com.itg.supplychainmanagment;
+package com.itg.supplychainmanagment.test;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
